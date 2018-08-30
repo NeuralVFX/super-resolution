d8c47e2898bfa52721b54e5d9ee85b1008e5576f
NeuralVFX <vfxhoneybadger@gmail.com>
2018-08-30 12:57:25 +0700

    Making script for full imagenet download

---
+++ data/get_full_imagenet.sh
@@ -0,0 +1,4 @@
+mkdir imagenet_full
+cd imagenet_full
+wget -O list_of_images.txt "http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n02139199"
+wget --timeout=1 --tries =2 -i list_of_images.txt
